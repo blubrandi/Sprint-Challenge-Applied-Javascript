a15881b7fd38f6b35b52e4b95115facea8e69811
blubrandi <blubrandi0@gmail.com>
2019-02-09 14:16:03 -0700

    Completed Stretch - added carousel and fade animation. Final commit.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,44 @@
-class Carousel {
 
+class Carousel {
+    constructor(carouselElement){
+        this.carousel = carouselElement;
+        this.leftButton = this.carousel.querySelector('.left-button')
+        this.rightButton = this.carousel.querySelector('.right-button')
+        this.images = this.carousel.querySelectorAll('img')
+        this.index = 0
+        this.images[this.index].style.display = 'flex'
+        this.rightButton.addEventListener('click', () => {this.scrollRight()})
+        this.leftButton.addEventListener('click', () => {this.scrollLeft()})   
+
+    }
+
+    scrollRight(){
+        this.index++
+        if(this.index === 4){
+            this.index = 0
+        }
+        this.images.forEach(image => {
+            image.style.display = 'none'
+        this.images[this.index].style.display = 'flex'
+        })
+    }
+
+    scrollLeft(){
+        this.index--
+        if(this.index < 0){
+            this.index = 3
+        }
+        this.images.forEach(image => {
+            image.style.display = 'none'
+        })
+        this.images[this.index].style.display = 'flex'
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel')
+let newCarousel = new Carousel(carousel)
+
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
